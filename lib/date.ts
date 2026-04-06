6870f8f0f6816be41f2448da41a706cd193090c4
Mohammer5 <jgs.salomon@gmail.com>
2026-04-06 08:10:04 +0800

    empty everything

--- lib/date.ts
+++
@@ -1,8 +0,0 @@
-export function formatDisplayDate(dateString: string) {
-  return new Intl.DateTimeFormat("en", {
-    day: "numeric",
-    month: "short",
-    year: "numeric",
-    timeZone: "Asia/Manila",
-  }).format(new Date(dateString));
-}
